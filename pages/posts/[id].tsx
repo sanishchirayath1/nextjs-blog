afa5b5284e966dd51d0d8b49a4c49ebf56e5fff2
sanishchirayath1 <sanishchirayath@gmail.com>
2022-01-05 18:38:45 +0530

    covert to typescript

--- pages/posts/[id].tsx
+++ pages/posts/[id].tsx
@@ -1,19 +1,19 @@
-import utilStyles from '../../styles/utils.module.css'
 import Layout from '../../components/Layout'
 import { getAllPostIds, getPostData } from '../../lib/posts'
 import Head from 'next/head'
 import Date from '../../components/date'
+import utilStyles from '../../styles/utils.module.css'
+import { GetStaticProps, GetStaticPaths } from 'next'
 
-export async function getStaticProps({ params }) {
-  const postData = await getPostData(params.id)
-  return {
-    props: {
-      postData
-    }
+export default function Post({
+  postData
+}: {
+  postData: {
+    title: string
+    date: string
+    contentHtml: string
   }
-}
-
-export default function Post({ postData }) {
+}) {
   return (
     <Layout>
       <Head>
@@ -30,10 +30,19 @@ export default function Post({ postData }) {
   )
 }
 
-export async function getStaticPaths() {
+export const getStaticPaths: GetStaticPaths = async () => {
   const paths = getAllPostIds()
   return {
     paths,
     fallback: false
+  }
+}
+
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const postData = await getPostData(params.id as string)
+  return {
+    props: {
+      postData
+    }
   }
 }
