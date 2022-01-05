c7c0d5bb4b026b2306b9c4bb697c9a037c06d54f
sanishchirayath1 <sanishchirayath@gmail.com>
2022-01-05 15:40:44 +0530

    UX for route Nav

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,8 +1,11 @@
+import utilStyles from '../../styles/utils.module.css'
 import Layout from '../../components/Layout'
 import { getAllPostIds, getPostData } from '../../lib/posts'
+import Head from 'next/head'
+import Date from '../../components/date'
 
 export async function getStaticProps({ params }) {
-  const postData = getPostData(params.id)
+  const postData = await getPostData(params.id)
   return {
     props: {
       postData
@@ -11,16 +14,21 @@ export async function getStaticProps({ params }) {
 }
 
 export default function Post({ postData }) {
-    return (
-      <Layout>
-        {postData.title}
-        <br />
-        {postData.id}
-        <br />
-        {postData.date}
-      </Layout>
-    )
-  }
+  return (
+    <Layout>
+      <Head>
+        <title>{postData.title}</title>
+      </Head>
+      <article>
+        <h1 className={utilStyles.headingXl}>{postData.title}</h1>
+        <div className={utilStyles.lightText}>
+          <Date dateString={postData.date} />
+        </div>
+        <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+      </article>
+    </Layout>
+  )
+}
 
 export async function getStaticPaths() {
   const paths = getAllPostIds()
